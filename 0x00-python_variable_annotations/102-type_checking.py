e28368946d3328aab5b4cc889e83f9badc9b8d86
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-08-01 21:02:17 +0300

    12. Type Checking

---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Module"""
+
+from typing import List
+
+
+def zoom_array(lst: List[int], factor: int = 2) -> List[int]:
+    """zoom_array"""
+    zoomed_in: List[int] = [item for item in lst for i in range(factor)]
+    return zoomed_in
+
+
+array = [12, 72, 91]
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, 3)
