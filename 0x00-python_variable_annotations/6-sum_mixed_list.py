fd6450f9fd39c9091f88ead98c07eb74122e6a32
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-08-01 19:30:58 +0300

    6. Complex types - mixed list

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""SUM Mixed List module"""
+from typing import List
+
+
+def mxd_lst(mxd_lst: List[int, float]) -> float:
+    """mxd_lst function"""
+    return sum(mxd_lst)
