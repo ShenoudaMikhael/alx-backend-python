40372c7c32012c88a1dc31329fcd1dd8fe8eb6a2
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-08-26 22:39:30 +0300

    5. Mocking a property

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -2,10 +2,7 @@
 """test_client"""
 import unittest
 from typing import Dict
-from unittest.mock import (
-    MagicMock,
-    patch,
-)
+from unittest.mock import MagicMock, patch, PropertyMock
 from parameterized import parameterized
 from client import GithubOrgClient
 
@@ -27,4 +24,19 @@ class TestGithubOrgClient(unittest.TestCase):
         mocked_fn.return_value = MagicMock(return_value=response)
         ghdotorg_c = GithubOrgClient(org)
         self.assertEqual(ghdotorg_c.org(), response)
-        mocked_fn.assert_called_once_with("https://api.github.com/orgs/{}".format(org))
+        mocked_fn.assert_called_once_with(
+            "https://api.github.com/orgs/{}".format(org))
+
+    def test_public_repos_url(self) -> None:
+        """test_public_repos_url"""
+        with patch(
+            "client.GithubOrgClient.org",
+            new_callable=PropertyMock,
+        ) as m_o:
+            m_o.return_value = {
+                "repos_url": "https://api.github.com/users/google/repos",
+            }
+            self.assertEqual(
+                GithubOrgClient("google")._public_repos_url,
+                "https://api.github.com/users/google/repos",
+            )
